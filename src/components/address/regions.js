661c7f4fe50b095d78555bb2bcb828320662a088
tuanpinggo <tuannh@pinggo.vn>
2024-10-03 10:51:53 +0700

    fix favicon region

--- src/components/address/regions.js
+++ src/components/address/regions.js
@@ -1,317 +1,317 @@
 export const regions = [
     {
-        id:1,
-        name: "Hà Nội",
-        region_id: 201,
+        "id": 64,
+        "name": "Hà Nội",
+        "region_id": 201
     },
     {
-        id:2,
-        name: "Hồ Chí Minh",
-        region_id: 202,
+        "id": 65,
+        "name": "Hồ Chí Minh",
+        "region_id": 202
     },
     {
-        id:3,
-        name: "Đà Nẵng",
-        region_id: 203,
+        "id": 66,
+        "name": "Đà Nẵng",
+        "region_id": 203
     },
     {
-        id:4,
-        name: "Đồng Nai",
-        region_id: 204,
+        "id": 67,
+        "name": "Đồng Nai",
+        "region_id": 204
     },
     {
-        id:5,
-        name: "Bình Dương",
-        region_id: 205,
+        "id": 68,
+        "name": "Bình Dương",
+        "region_id": 205
     },
     {
-        id:6,
-        name: "Bà Rịa - Vũng Tàu",
-        region_id: 206,
+        "id": 69,
+        "name": "Bà Rịa - Vũng Tàu",
+        "region_id": 206
     },
     {
-        id:7,
-        name: "Gia Lai",
-        region_id: 207,
+        "id": 70,
+        "name": "Gia Lai",
+        "region_id": 207
     },
     {
-        id:8,
-        name: "Khánh Hòa",
-        region_id: 208,
+        "id": 71,
+        "name": "Khánh Hòa",
+        "region_id": 208
     },
     {
-        id:9,
-        name: "Lâm Đồng",
-        region_id: 209,
+        "id": 72,
+        "name": "Lâm Đồng",
+        "region_id": 209
     },
     {
-        id:10,
-        name: "Đắk Lắk",
-        region_id: 210,
+        "id": 73,
+        "name": "Đắk Lắk",
+        "region_id": 210
     },
     {
-        id:11,
-        name: "Long An",
-        region_id: 211,
+        "id": 74,
+        "name": "Long An",
+        "region_id": 211
     },
     {
-        id:12,
-        name: "Tiền Giang",
-        region_id: 212,
+        "id": 75,
+        "name": "Tiền Giang",
+        "region_id": 212
     },
     {
-        id:13,
-        name: "Bến Tre",
-        region_id: 213,
+        "id": 76,
+        "name": "Bến Tre",
+        "region_id": 213
     },
     {
-        id:14,
-        name: "Trà Vinh",
-        region_id: 214,
+        "id": 77,
+        "name": "Trà Vinh",
+        "region_id": 214
     },
     {
-        id:15,
-        name: "Vĩnh Long",
-        region_id: 215,
+        "id": 78,
+        "name": "Vĩnh Long",
+        "region_id": 215
     },
     {
-        id:16,
-        name: "Đồng Tháp",
-        region_id: 216,
+        "id": 79,
+        "name": "Đồng Tháp",
+        "region_id": 216
     },
     {
-        id:17,
-        name: "An Giang",
-        region_id: 217,
+        "id": 80,
+        "name": "An Giang",
+        "region_id": 217
     },
     {
-        id:18,
-        name: "Sóc Trăng",
-        region_id: 218,
+        "id": 81,
+        "name": "Sóc Trăng",
+        "region_id": 218
     },
     {
-        id:19,
-        name: "Kiên Giang",
-        region_id: 219,
+        "id": 82,
+        "name": "Kiên Giang",
+        "region_id": 219
     },
     {
-        id:20,
-        name: "Cần Thơ",
-        region_id: 220,
+        "id": 83,
+        "name": "Cần Thơ",
+        "region_id": 220
     },
     {
-        id:21,
-        name: "Vĩnh Phúc",
-        region_id: 221,
+        "id": 84,
+        "name": "Vĩnh Phúc",
+        "region_id": 221
     },
     {
-        id:22,
-        name: "Thừa Thiên Huế",
-        region_id: 223,
+        "id": 85,
+        "name": "Thừa Thiên Huế",
+        "region_id": 223
     },
     {
-        id:23,
-        name: "Hải Phòng",
-        region_id: 224,
+        "id": 86,
+        "name": "Hải Phòng",
+        "region_id": 224
     },
     {
-        id:24,
-        name: "Hải Dương",
-        region_id: 225,
+        "id": 87,
+        "name": "Hải Dương",
+        "region_id": 225
     },
     {
-        id:25,
-        name: "Thái Bình",
-        region_id: 226,
+        "id": 88,
+        "name": "Thái Bình",
+        "region_id": 226
     },
     {
-        id:26,
-        name: "Hà Giang",
-        region_id: 227,
+        "id": 89,
+        "name": "Hà Giang",
+        "region_id": 227
     },
     {
-        id:27,
-        name: "Tuyên Quang",
-        region_id: 228,
+        "id": 90,
+        "name": "Tuyên Quang",
+        "region_id": 228
     },
     {
-        id:28,
-        name: "Phú Thọ",
-        region_id: 229,
+        "id": 91,
+        "name": "Phú Thọ",
+        "region_id": 229
     },
     {
-        id:29,
-        name: "Quảng Ninh",
-        region_id: 230,
+        "id": 92,
+        "name": "Quảng Ninh",
+        "region_id": 230
     },
     {
-        id:30,
-        name: "Nam Định",
-        region_id: 231,
+        "id": 93,
+        "name": "Nam Định",
+        "region_id": 231
     },
     {
-        id:31,
-        name: "Hà Nam",
-        region_id: 232,
+        "id": 94,
+        "name": "Hà Nam",
+        "region_id": 232
     },
     {
-        id:32,
-        name: "Ninh Bình",
-        region_id: 233,
+        "id": 95,
+        "name": "Ninh Bình",
+        "region_id": 233
     },
     {
-        id:33,
-        name: "Thanh Hóa",
-        region_id: 234,
+        "id": 96,
+        "name": "Thanh Hóa",
+        "region_id": 234
     },
     {
-        id:34,
-        name: "Nghệ An",
-        region_id: 235,
+        "id": 97,
+        "name": "Nghệ An",
+        "region_id": 235
     },
     {
-        id:35,
-        name: "Hà Tĩnh",
-        region_id: 236,
+        "id": 98,
+        "name": "Hà Tĩnh",
+        "region_id": 236
     },
     {
-        id:36,
-        name: "Quảng Bình",
-        region_id: 237,
+        "id": 99,
+        "name": "Quảng Bình",
+        "region_id": 237
     },
     {
-        id:37,
-        name: "Quảng Trị",
-        region_id: 238,
+        "id": 100,
+        "name": "Quảng Trị",
+        "region_id": 238
     },
     {
-        id:38,
-        name: "Bình Phước",
-        region_id: 239,
+        "id": 101,
+        "name": "Bình Phước",
+        "region_id": 239
     },
     {
-        id:39,
-        name: "Tây Ninh",
-        region_id: 240,
+        "id": 102,
+        "name": "Tây Ninh",
+        "region_id": 240
     },
     {
-        id:40,
-        name: "Đắk Nông",
-        region_id: 241,
+        "id": 103,
+        "name": "Đắk Nông",
+        "region_id": 241
     },
     {
-        id:41,
-        name: "Quảng Ngãi",
-        region_id: 242,
+        "id": 104,
+        "name": "Quảng Ngãi",
+        "region_id": 242
     },
     {
-        id:42,
-        name: "Quảng Nam",
-        region_id: 243,
+        "id": 105,
+        "name": "Quảng Nam",
+        "region_id": 243
     },
     {
-        id:43,
-        name: "Thái Nguyên",
-        region_id: 244,
+        "id": 106,
+        "name": "Thái Nguyên",
+        "region_id": 244
     },
     {
-        id:44,
-        name: "Bắc Kạn",
-        region_id: 245,
+        "id": 107,
+        "name": "Bắc Kạn",
+        "region_id": 245
     },
     {
-        id:45,
-        name: "Cao Bằng",
-        region_id: 246,
+        "id": 108,
+        "name": "Cao Bằng",
+        "region_id": 246
     },
     {
-        id:46,
-        name: "Lạng Sơn",
-        region_id: 247,
+        "id": 109,
+        "name": "Lạng Sơn",
+        "region_id": 247
     },
     {
-        id:47,
-        name: "Bắc Giang",
-        region_id: 248,
+        "id": 110,
+        "name": "Bắc Giang",
+        "region_id": 248
     },
     {
-        id:48,
-        name: "Bắc Ninh",
-        region_id: 249,
+        "id": 111,
+        "name": "Bắc Ninh",
+        "region_id": 249
     },
     {
-        id:49,
-        name: "Hậu Giang",
-        region_id: 250,
+        "id": 112,
+        "name": "Hậu Giang",
+        "region_id": 250
     },
     {
-        id:50,
-        name: "Cà Mau",
-        region_id: 252,
+        "id": 113,
+        "name": "Cà Mau",
+        "region_id": 252
     },
     {
-        id:51,
-        name: "Bạc Liêu",
-        region_id: 253,
+        "id": 114,
+        "name": "Bạc Liêu",
+        "region_id": 253
     },
     {
-        id:52,
-        name: "Bình Thuận",
-        region_id: 258,
+        "id": 115,
+        "name": "Bình Thuận",
+        "region_id": 258
     },
     {
-        id:53,
-        name: "Kon Tum",
-        region_id: 259,
+        "id": 116,
+        "name": "Kon Tum",
+        "region_id": 259
     },
     {
-        id:54,
-        name: "Phú Yên",
-        region_id: 260,
+        "id": 117,
+        "name": "Phú Yên",
+        "region_id": 260
     },
     {
-        id:55,
-        name: "Ninh Thuận",
-        region_id: 261,
+        "id": 118,
+        "name": "Ninh Thuận",
+        "region_id": 261
     },
     {
-        id:56,
-        name: "Bình Định",
-        region_id: 262,
+        "id": 119,
+        "name": "Bình Định",
+        "region_id": 262
     },
     {
-        id:57,
-        name: "Yên Bái",
-        region_id: 263,
+        "id": 120,
+        "name": "Yên Bái",
+        "region_id": 263
     },
     {
-        id:58,
-        name: "Lai Châu",
-        region_id: 264,
+        "id": 121,
+        "name": "Lai Châu",
+        "region_id": 264
     },
     {
-        id:59,
-        name: "Điện Biên",
-        region_id: 265,
+        "id": 122,
+        "name": "Điện Biên",
+        "region_id": 265
     },
     {
-        id:60,
-        name: "Sơn La",
-        region_id: 266,
+        "id": 123,
+        "name": "Sơn La",
+        "region_id": 266
     },
     {
-        id:61,
-        name: "Hòa Bình",
-        region_id: 267,
+        "id": 124,
+        "name": "Hòa Bình",
+        "region_id": 267
     },
     {
-        id:62,
-        name: "Hưng Yên",
-        region_id: 268,
+        "id": 125,
+        "name": "Hưng Yên",
+        "region_id": 268
     },
     {
-        id:63,
-        name: "Lào Cai",
-        region_id: 269,
+        "id": 126,
+        "name": "Lào Cai",
+        "region_id": 269
     }
 ]
